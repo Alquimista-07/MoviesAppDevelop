4a0c3130d803ada0e52ca11ee641d14488a889aa
Alquimista-07 <jdca.07@gmail.com>
2023-12-02 17:34:25 -0500

    Implementado ordenamiento

--- movies-app-frontend/src/app/movies/pages/listado/listado.component.ts
+++ movies-app-frontend/src/app/movies/pages/listado/listado.component.ts
@@ -11,11 +11,53 @@ export class ListadoComponent {
   // Propiedad para las películas
   movies: Movie[] = [];
 
+  selected = '';
+
   constructor(private moviesService: MoviesService){ }
 
   ngOnInit(): void {
     this.moviesService.getMovies()
         .subscribe(movies => this.movies = movies);
+  }
+
+  ordenar(selected: string){
+
+    console.log(this.movies);
+
+    switch (selected) {
+      case 'title':
+        this.ordenarTitulo();
+        break;
+      
+      case 'releasedDate':
+        this.ordenarFecha();
+        break;
+    
+      default:
+        break;
+    }
+  }
+
+  ordenarTitulo(){
+    this.movies.sort(function( a, b ) {
+      if(a.title < b.title)
+        return -1;
+      else if (a.title > b.title)
+        return 1;
+      else
+        return 0;
+    });
+  }
+
+  ordenarFecha(){
+    this.movies.sort(function( a, b ) {
+      if(a.releasedDate.year < b.releasedDate.year)
+        return -1;
+      else if (a.releasedDate.year > b.releasedDate.year)
+        return 1;
+      else
+        return 0;
+    });
   }
 
 }
