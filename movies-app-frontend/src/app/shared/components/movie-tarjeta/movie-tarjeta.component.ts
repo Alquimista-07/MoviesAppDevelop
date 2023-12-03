7b499943d95469b4da5771ac5cb4791c69139812
Alquimista-07 <jdca.07@gmail.com>
2023-12-03 14:36:17 -0500

    Ajuste para ir a la pantalla con la descripcion de la película dependiendo de la ruta si esta autenticado o no

--- movies-app-frontend/src/app/shared/components/movie-tarjeta/movie-tarjeta.component.ts
+++ movies-app-frontend/src/app/shared/components/movie-tarjeta/movie-tarjeta.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Movie } from '../../interfaces/movies.interface';
 import { MoviesService } from '../../../movies/services/movies.service';
 import Swal from 'sweetalert2';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-movie-tarjeta',
@@ -16,7 +17,11 @@ export class MovieTarjetaComponent {
 
   @Input() movie!: Movie;
 
-  constructor(private moviesService: MoviesService){ }
+  constructor(private moviesService: MoviesService, private authService: AuthService){ }
+
+  get auth() {
+    return this.authService.auth;
+  }
 
   addFavorite(movie: Movie){
     this.moviesService.addFavorite(movie);
