8855134d233aa4e361ec43a60d8abe443beafbc7
Alquimista-07 <jdca.07@gmail.com>
2023-12-06 07:33:31 -0500

    Ajuste para visualizar tag de favorito en los card de la pantalla principal

--- movies-app-frontend/src/app/shared/components/movie-tarjeta/movie-tarjeta.component.ts
+++ movies-app-frontend/src/app/shared/components/movie-tarjeta/movie-tarjeta.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, AfterContentInit, AfterContentChecked } from '@angular/core';
 import { Movie } from '../../interfaces/movies.interface';
 import { MoviesService } from '../../../movies/services/movies.service';
 import Swal from 'sweetalert2';
@@ -14,15 +14,25 @@ import { Router } from '@angular/router';
     }
   `]
 })
-export class MovieTarjetaComponent {
+export class MovieTarjetaComponent implements AfterContentChecked {
 
   @Input() movie!: Movie;
   _favorites: Movie[] = [];
+  isFavorite: boolean = false;
 
-  constructor( private authService: AuthService ){ }
+  constructor( private authService: AuthService, private moviesService: MoviesService ){ }
+  
+  ngAfterContentChecked(): void {
+    this.validaFavorito(this.movie.id);
+  }
 
   get auth() {
     return this.authService.auth;
+  }
+
+  validaFavorito(idFavorito: string){
+    this.isFavorite = false;
+    this.isFavorite = this.moviesService.validarFavorito(idFavorito);
   }
 
 }
